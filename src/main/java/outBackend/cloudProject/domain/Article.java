fd651460f56f85e6d1ceeb3fba9f33ddfc70e04b
용성 <ehfrhfo9494@naver.com>
2024-06-09 23:38:04 +0900

    프로젝트 생성
    POST 형식(테스트 필요)

---
+++ src/main/java/outBackend/cloudProject/domain/Article.java
@@ -0,0 +1,59 @@
+package outBackend.cloudProject.domain;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String title;
+
+    @Column(name = "body")
+    private String content;
+
+    @Column(name = "front_recruit_count")
+    private Integer front_recruit_count;
+
+    @Column(name = "front_current_count")
+    private Integer front_current_count;
+
+    @Column(name = "back_recruit_count")
+    private  Integer back_recruit_count;
+
+    @Column(name = "back_current_count")
+    private  Integer back_current_count;
+
+    @Column(name = "design_recruit_count")
+    private Integer design_recruit_count;
+
+    @Column(name = "design_current_count")
+    private Integer design_current_count;
+
+    @Builder
+    public Article(Long id, String title, String content, Integer front_recruit_count, Integer front_current_count
+            ,Integer back_recruit_count, Integer back_current_count, Integer design_recruit_count, Integer design_current_count) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.front_recruit_count = front_recruit_count;
+        this.front_current_count = front_current_count;
+        this.back_recruit_count = back_recruit_count;
+        this.back_current_count = back_current_count;
+        this.design_recruit_count = design_recruit_count;
+        this.design_current_count = design_current_count;
+    }
+}
+
+
+
