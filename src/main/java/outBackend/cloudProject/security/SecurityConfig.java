d1d4775dd99d61a8ae437435203b6610f2cb5f6b
SJH-00 <tlstkd437@gmail.com>
2024-06-11 03:14:42 +0900

    refactor: 개발 편의 위해 모든 api 토큰 없이 허용

--- src/main/java/outBackend/cloudProject/security/SecurityConfig.java
+++ src/main/java/outBackend/cloudProject/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
 
                 // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
                 .authorizeHttpRequests(ahr -> ahr
-                        .requestMatchers("/user/**").permitAll()
+                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated())   // 나머지 API 는 전부 인증 필요
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
