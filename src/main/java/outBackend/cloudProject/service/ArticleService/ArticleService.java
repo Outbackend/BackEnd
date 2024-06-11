e1c3e62937f497825fb50129a37f7746665b2928
용성 <ehfrhfo9494@naver.com>
2024-06-11 18:11:14 +0900

    api/article/{id} GET, DELETE
    테스트 자동화도 추가하였습니다.

--- src/main/java/outBackend/cloudProject/service/ArticleService/ArticleService.java
+++ src/main/java/outBackend/cloudProject/service/ArticleService/ArticleService.java
@@ -1,7 +1,10 @@
 package outBackend.cloudProject.service.ArticleService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import outBackend.cloudProject.domain.Article;
 import outBackend.cloudProject.dto.AddArticleRequest;
 import outBackend.cloudProject.repository.ArticleRepository;
@@ -18,5 +21,12 @@ public class ArticleService {
     }
     public List<Article> findAll(){
         return articleRepository.findAll();
+    }
+    public Article findById(long id){
+        return articleRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("not found: " + id));
+    }
+    public void delete(long id){
+        articleRepository.deleteById(id);
     }
 }
