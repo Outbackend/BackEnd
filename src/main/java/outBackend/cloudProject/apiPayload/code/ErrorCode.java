ef6579b6b32cf62748b691f9ce683dc455422da1
용성 <ehfrhfo9494@naver.com>
2024-06-11 17:50:46 +0900

    로컬DB사용

--- src/main/java/outBackend/cloudProject/apiPayload/code/ErrorCode.java
+++
@@ -1,6 +0,0 @@
-package outBackend.cloudProject.apiPayload.code;
-
-public interface ErrorCode {
-
-    public ErrorReasonDTO getReason();
-}
--- src/main/java/outBackend/cloudProject/domain/Comment.java
+++
@@ -1,7 +0,0 @@
-package outBackend.cloudProject.domain;
-
-public class Comment {
-
-    private Long id;
-    private String body;
-}
